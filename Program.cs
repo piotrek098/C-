4a7d5e653c8340acbfc294c1b44acec0870a1258
piotrek098 <123263493+piotrek098@users.noreply.github.com>
2023-10-23 17:21:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// Zadanie 2.1
+/*double C, F;
+Console.WriteLine("Podaj liczbe w stopniach Celsjusza");
+C = double.Parse(Console.ReadLine());
+F =(9D/5)*C+32;
+Console.WriteLine(F);
+Console.ReadKey();
+*/
+//Zadanie 2.2
+/*double a, b, c,x;
+Console.WriteLine("Podaj współczynniki a,b,c");
+a = double.Parse(Console.ReadLine());
+b = double.Parse(Console.ReadLine());
+c= double.Parse(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+double delta = Math.Pow(b, 2) - 4 * a * c;
+Console.WriteLine(delta);
+*/
+// Zadanie 2.3
+/*Console.WriteLine("podaj swój wzrost w metrach");
+double wzrost = double.Parse(Console.ReadLine());
+Console.WriteLine("podaj swoją wage");
+double waga = double.Parse(Console.ReadLine());
+
+double BMI = (double)waga / Math.Pow(wzrost, 2);
+
+Console.WriteLine(BMI);
+
+Console.ReadLine();
+*/
+
+// zadanie 2.4 odpowiedz A
+//int x = 100;
+//Console.WriteLine(++x * 2);
+
+
+/*Zadanie 2.5 odpowiedz D
+ int x = 2, y = 3;
+x *= y * 2;
+Console.WriteLine(x);
+
+Zadanie 2.5 odpowiedz D
+*/
+
+/*Zadnie 2.6 
+    Odpowiedz 3
+int x, y = 4;
+x = (y -= 2);
+x = y++;
+x = y--;
+Console.WriteLine(x);
+*/
+
+/* Zadanie 2.7  odpowiedz 7
+int x, y = 5;
+x = ++y * 2;
+x = y++;
+x = y--;
+Console.WriteLine(++y);
+*/
+
+/* Zadanie 2.8 odpowiedz c
+bool x;
+int y = 1, z = 1;
+x = (y == 1 && z++ == 1);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+*/
+
+/* zadanie 2.10
+int powierzchnia = 100, osoby = 10;
+double gestoscZaludnienia =(double) osoby / powierzchnia;
+Console.WriteLine(gestoscZaludnienia);
+*/
